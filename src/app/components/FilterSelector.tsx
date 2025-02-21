d48b294415562270ef7e9d97882a7a82475bc7bf
Niklas-Nordin <niklas.nordin@chasacademy.se>
2025-02-21 11:24:37 +0100

    Added some more filter options and also added some styling to those.

--- src/app/components/FilterSelector.tsx
+++ src/app/components/FilterSelector.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import type { RootState } from "../redux/store";
 import { RenderList, Jobs } from "./ListItem";
 import "../css/ListItem.css";
+import "../css/filterSelector.css";
 
 import {
   updateCompany,
@@ -10,7 +11,6 @@ import {
   updateContract,
   updateLanguages,
   updateLocation,
-  updateSearchTerm,
 } from "../redux/slices/filterSelectionSlice";
 
 function FilterSelector({ jobList }: { jobList: Jobs[] }) {
@@ -19,18 +19,36 @@ function FilterSelector({ jobList }: { jobList: Jobs[] }) {
 
   const companies = [...new Set(jobList.map((job) => job.company))];
   const roles = [...new Set(jobList.map((job) => job.role))];
-  console.log(companies);
-  console.log(roles);
+  const contracts = [...new Set(jobList.map((job) => job.contract))];
+  const languages = [...new Set(jobList.map((job) => job.languages))];
+  const locations = [...new Set(jobList.map((job) => job.location))];
 
   const filterJobs = jobList.filter((jobs) => {
     return (
       (filterState.company ? jobs.company === filterState.company : true) &&
       (filterState.role ? jobs.role === filterState.role : true) &&
+      (filterState.contract ? jobs.contract === filterState.contract : true) &&
+      (filterState.languages
+        ? jobs.languages === filterState.languages.split("")
+        : true) &&
+      (filterState.location ? jobs.location === filterState.location : true) &&
       (filterState.searchTerm
         ? jobs.company
             .toLowerCase()
             .includes(filterState.searchTerm.toLowerCase()) ||
-          jobs.role.toLowerCase().includes(filterState.searchTerm.toLowerCase())
+          jobs.role
+            .toLowerCase()
+            .includes(filterState.searchTerm.toLowerCase()) ||
+          jobs.contract
+            .toLowerCase()
+            .includes(filterState.searchTerm.toLowerCase()) ||
+          jobs.languages
+            .toString()
+            .toLowerCase()
+            .includes(filterState.searchTerm.toLowerCase()) ||
+          jobs.location
+            .toLowerCase()
+            .includes(filterState.searchTerm.toLowerCase())
         : true)
     );
   });
@@ -38,35 +56,94 @@ function FilterSelector({ jobList }: { jobList: Jobs[] }) {
   return (
     <div>
       <div className="flex justify-center items-center gap-4">
-        <div>
+        <div className="dropdown-menu">
           <label htmlFor="company">Companies</label>
           <select
             id="company"
             value={filterState.company}
             onChange={(e) => dispatch(updateCompany(e.target.value))}
-            className="w-full"
+            className="input-field w-full"
           >
-            <option value="">All</option>
+            <option className="option" value="">
+              All
+            </option>
             {companies.map((company, index) => (
-              <option key={index} value={company}>
+              <option className="option" key={index} value={company}>
                 {company}
               </option>
             ))}
           </select>
         </div>
 
-        <div>
+        <div className="dropdown-menu">
           <label htmlFor="role">Roles</label>
           <select
             id="role"
             value={filterState.role}
             onChange={(e) => dispatch(updateRole(e.target.value))}
-            className="w-full"
+            className="input-field w-full"
           >
-            <option value="">All</option>
+            <option className="option" value="">
+              All
+            </option>
             {roles.map((role, index) => (
-              <option key={index} value={role}>
+              <option className="option" key={index} value={role}>
                 {role}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="dropdown-menu">
+          <label htmlFor="contract">Contract</label>
+          <select
+            id="contract"
+            value={filterState.contract}
+            onChange={(e) => dispatch(updateContract(e.target.value))}
+            className="input-field w-full"
+          >
+            <option className="option" value="">
+              All
+            </option>
+            {contracts.map((contract, index) => (
+              <option className="option" key={index} value={contract}>
+                {contract}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="dropdown-menu">
+          <label htmlFor="languages">Languages</label>
+          <select
+            id="languages"
+            value={filterState.languages}
+            onChange={(e) => dispatch(updateLanguages(e.target.value))}
+            className="input-field w-full"
+          >
+            <option className="option" value="">
+              All
+            </option>
+            {languages.map((language, index) => (
+              <option className="option" key={index} value={language}>
+                {language}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="dropdown-menu">
+          <label htmlFor="location">Location</label>
+          <select
+            id="location"
+            value={filterState.location}
+            onChange={(e) => dispatch(updateLocation(e.target.value))}
+            className="input-field w-full"
+          >
+            <option className="option" value="">
+              All
+            </option>
+            {locations.map((location, index) => (
+              <option className="option" key={index} value={location}>
+                {location}
               </option>
             ))}
           </select>
